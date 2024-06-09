a84b21cacdb40c8e4cfc90dd21992359243160c3
whabey <hassan@gmail.com>
2024-06-09 20:54:33 +0300

    hassan ahemd

--- git_commands.sh
+++ git_commands.sh
@@ -1,2 +1,8 @@
 git add -A
 git commit -m "hassan ahemd"
+git branch optional
+git branch optional
+git remote add origin https://github.com/Abdelwahabm/Git_Tool
+git push -u -f origin main
+git remote add origin https://github.com/Abdelwahabm/Git_Tool
+git push -u -f origin main
