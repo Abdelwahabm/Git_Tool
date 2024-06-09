c52ee04e59c729fe89e3a7d407cc04ad8dcea07e
whabey <hassan@gmail.com>
2024-06-09 20:54:19 +0300

    hassan ahemd

--- git_commands.sh
+++ git_commands.sh
@@ -1,9 +1,2 @@
-git config --global user.name "whabey"
-git config --global user.email "hassan@gmail.com"
-git init
 git add -A
 git commit -m "hassan ahemd"
-git remote add origin https://github.com/Abdelwahabm/Git_Tool
-git push -u -f origin main
-git remote add origin https://github.com/Abdelwahabm/Git_Tool
-git push -u -f origin main
