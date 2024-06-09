711bb5a28c60c648c5d5cd7ab8edc9818415b4eb
sqw <ewd@gma.com>
2024-06-09 22:37:42 +0300

    ananan

--- git_commands.sh
+++ git_commands.sh
@@ -1,8 +1,7 @@
+git config --global user.name "sqw"
+git config --global user.email "ewd@gma.com"
+git init
 git add -A
-git commit -m "hassan ahemd"
-git branch optional
-git branch optional
+git commit -m "ananan"
 git remote add origin https://github.com/Abdelwahabm/Git_Tool
-git push -u -f origin main
-git remote add origin https://github.com/Abdelwahabm/Git_Tool
-git push -u -f origin main
+git push -u -f origin master
